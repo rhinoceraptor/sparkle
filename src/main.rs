6ce2f5bcb62eb18b9a6b8b8680422fa1fd6d5b7e
Jack Lewis <jack@jacklew.is>
2025-05-08 20:45:46 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,49 @@
+use std::thread;
+use std::time::Duration;
+use std::error::Error;
 use esp_idf_hal::delay::Ets;
-use esp_idf_hal::gpio::{Gpio18, Gpio19, Gpio23, Gpio5, Output, PinDriver};
+use esp_idf_hal::units::FromValueType;
+use esp_idf_hal::gpio::{AnyInputPin, PinDriver};
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_hal::spi::*;
 use ssd1306::{prelude::*, Ssd1306};
-use display_interface_spi::SPIInterfaceNoCS;
+use display_interface_spi::SPIInterface;
 
 use embedded_graphics::{
-    mono_font::{ascii::FONT_6X10, MonoTextStyle},
+    mono_font::{ascii::*, MonoTextStyle},
     pixelcolor::BinaryColor,
     prelude::*,
     text::Text,
+    primitives::{PrimitiveStyle, Rectangle},
+    Drawable,
 };
 
-fn main() -> anyhow::Result<()> {
+fn main() -> anyhow::Result<(), Box<dyn Error>> {
     // Initialize peripherals
     let peripherals = Peripherals::take()?;
     let spi = peripherals.spi2;
-    let sclk = peripherals.pins.gpio18; // SPI CLK
-    let mosi = peripherals.pins.gpio23; // SPI MOSI
-    let cs = peripherals.pins.gpio5;    // Chip Select
-    let dc = peripherals.pins.gpio19;   // Data/Command
-    let rst = peripherals.pins.gpio21;  // Reset (optional)
+
+    let mut rst  = PinDriver::output(peripherals.pins.gpio21)?;
+    let dc   = PinDriver::output(peripherals.pins.gpio19)?;
+    let sclk = peripherals.pins.gpio18;
+    let sda  = peripherals.pins.gpio23;
+    let cs   = peripherals.pins.gpio5;
+
+    let config = config::Config::new()
+        .baudrate(10.MHz().into());
 
     // Initialize SPI
-    let spi = SpiDeviceDriver::new_single(
+    let device = SpiDeviceDriver::new_single(
         spi,
         sclk,
-        mosi,
-        None, // MISO not used
-        &SpiConfig::new().baudrate(10.MHz().into()),
+        sda,
+        Option::<AnyInputPin>::None,
+        Some(cs),
+        &SpiDriverConfig::new(),
+        &config,
     )?;
 
-    // Prepare pins
-    let mut dc = PinDriver::output(dc)?;
-    let mut rst = PinDriver::output(rst)?;
-    let cs = PinDriver::output(cs)?;
-
-    // Create SPI interface without automatic CS control
-    let spi_interface = SPIInterfaceNoCS::new(spi, dc);
+    let spi_interface = SPIInterface::new(device, dc);
 
     // Reset display manually
     rst.set_low()?;
@@ -49,11 +55,82 @@ fn main() -> anyhow::Result<()> {
     let mut display = Ssd1306::new(spi_interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
 
-    display.init()?;
-    display.flush()?;
+    display.init().unwrap();
+    display.flush().unwrap();
 
-    // Draw "Hello, world!"
-    let text_style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On);
-    Text::new("Hello, world!", Point::zero(), text_style).draw(&mut display)?;
-    display.flush()?;
+    let fonts = [
+        FONT_4X6,
+        FONT_5X7,
+        FONT_5X8,
+        FONT_6X10,
+        FONT_6X12,
+        FONT_6X13,
+        FONT_6X13_BOLD,
+        FONT_6X13_ITALIC,
+        FONT_6X9,
+        FONT_7X13,
+        FONT_7X13_BOLD,
+        FONT_7X13_ITALIC,
+        FONT_7X14,
+        FONT_7X14_BOLD,
+        FONT_8X13,
+        FONT_8X13_BOLD,
+        FONT_8X13_ITALIC,
+        FONT_9X15,
+        FONT_9X15_BOLD,
+        FONT_9X18,
+        FONT_9X18_BOLD,
+        FONT_10X20,
+    ];
+
+    let font_names = [
+        "FONT_4X6",
+        "FONT_5X7",
+        "FONT_5X8",
+        "FONT_6X10",
+        "FONT_6X12",
+        "FONT_6X13",
+        "FONT_6X13_BOLD",
+        "FONT_6X13_ITALIC",
+        "FONT_6X9",
+        "FONT_7X13",
+        "FONT_7X13_BOLD",
+        "FONT_7X13_ITALIC",
+        "FONT_7X14",
+        "FONT_7X14_BOLD",
+        "FONT_8X13",
+        "FONT_8X13_BOLD",
+        "FONT_8X13_ITALIC",
+        "FONT_9X15",
+        "FONT_9X15_BOLD",
+        "FONT_9X18",
+        "FONT_9X18_BOLD",
+        "FONT_10X20",
+    ];
+
+    let mut i = 0;
+
+    loop {
+        // Draw "Hello, world!"
+        let text_style = MonoTextStyle::new(&fonts[i], BinaryColor::On);
+        let text = format!("\n{}\nabcdefghijklmnopqrstuvwxyz\nNABCDEFGHIJKLMNOPQRSTUVWXYZ\n0123456789", &font_names[i]);
+        Text::new(&text, Point::zero(), text_style).draw(&mut display).unwrap();
+        display.flush().unwrap();
+        thread::sleep(Duration::from_millis(1000));
+        i += 1;
+        if i == fonts.len() {
+            i = 0;
+        }
+
+        let clear = Rectangle::new(
+            Point::new(0, 0),
+            display.bounding_box().size,
+        )
+        .into_styled(PrimitiveStyle::with_fill(BinaryColor::Off));
+
+        clear.draw(&mut display).unwrap();
+        display.flush().unwrap();
+    }
+
+    Ok(())
 }
